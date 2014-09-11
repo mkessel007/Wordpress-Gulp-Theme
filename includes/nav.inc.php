5024201036a662b54fac3b3eb8e5809d647ecb43
Jonjoe Whitfield <me@jonjoe.io>
2014-09-11 19:13:53 +0100

    Update nav.inc.php

---
+++ includes/nav.inc.php
@@ -0,0 +1,43 @@
+<nav role="navigation" class="navbar navbar-default main-nav">
+	<div class="container-fluid">
+		<div class="navbar-header">
+			<button type="button" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" class="navbar-toggle">
+				<span class="sr-only">Toggle navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+			<a href="#'" class="navbar-brand"></a>
+		</div>
+		<div id="bs-example-navbar-collapse-1" class="collapse navbar-collapse">
+			<ul class="nav navbar-nav">
+				<li class="active">
+					<a href="#">Home</a>
+				</li>
+				<li>
+					<a href="#">About Us</a>
+				</li>
+				<li class="dropbox">
+					<a href="#" data-toggle="dropdown" class="dropdown-toggle">Curriculum<b class="caret"></b></a>
+					<ul class="dropdown-menu">
+						<li><a href="#">Year R</a></li>
+						<li><a href="#">Year 1</a></li>
+						<li><a href="#">Year 2</a></li>
+					</ul>
+				</li>
+				<li>
+					<a href="#">Other</a>
+				</li>
+				<li>
+					<a href="#">Other</a>
+				</li>
+				<li>
+					<a href="#">Other</a>
+				</li>
+				<li>
+					<a href="#">Other</a>
+				</li>
+			</ul>
+		</div>
+	</div>
+</nav>
