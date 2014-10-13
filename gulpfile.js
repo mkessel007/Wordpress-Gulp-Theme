36d06127cb71821067e911ef1bde61030ac45d09
Jonjoe Whitfield <me@jonjoe.io>
2014-10-13 13:33:38 +0100

    JSLinter Changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,17 @@
-var gulp = require('gulp'); 
-
-var bower = require('bower');
-
-var compass    = require('gulp-compass');
-var coffee    = require('gulp-coffee');
-var jshint     = require('gulp-jshint');
-var concat     = require('gulp-concat');
-var uglify     = require('gulp-uglify');
-var prefix     = require('gulp-autoprefixer'); 
-var rename     = require('gulp-rename');
-var gutil      = require('gulp-util');
-var minifyCSS  = require('gulp-minify-css');
-var sourcemaps = require('gulp-sourcemaps');
-var images     = require('gulp-imagemin');
-var pngcrush = require('imagemin-pngcrush');
+var gulp = require("gulp"),
+		bower = require("bower"),
+		compass    = require("gulp-compass"),
+		coffee    = require("gulp-coffee"),
+		jshint     = require("gulp-jshint"),
+		concat     = require("gulp-concat"),
+		uglify     = require("gulp-uglify"),
+		prefix     = require("gulp-autoprefixer"),
+		rename     = require("gulp-rename"),
+		gutil      = require("gulp-util"),
+		minifyCSS  = require("gulp-minify-css"),
+		sourcemaps = require("gulp-sourcemaps"),
+		images     = require("gulp-imagemin"),
+		pngcrush = require("imagemin-pngcrush");
 
 globals = {
 	// Working Directory
@@ -23,13 +21,13 @@ globals = {
 };
 
 paths = {
-	app : {
+	app: {
 		"scripts": globals.dev + "/scripts",
 		"styles": globals.dev + "/styles",
 		"images": globals.dev + "/images",
 		"fonts": globals.dev + "/fonts"
 	},
-	dist : {
+	dist: {
 		"scripts": globals.prod + "/js",
 		"styles": globals.prod + "/css",
 		"images": globals.prod + "/img",
@@ -37,55 +35,52 @@ paths = {
 	}
 };
 
-gulp.task('fonts', function(){
-  gulp.src(paths.app.fonts + '/*')
+gulp.task("fonts", function() {
+  gulp.src(paths.app.fonts + "/*")
   .pipe(gulp.dest(paths.dist.fonts));
 });
 
-gulp.task('compass', function() {
-	gulp.src(paths.app.styles + '/*.scss')
+gulp.task("compass", function() {
+	gulp.src(paths.app.styles + "/*.scss")
 		.pipe(compass({
 			css: paths.dist.styles,
-			sass: paths.app.styles,
+			sass: paths.app.styles
 		}))
 		.pipe(prefix())
 		.pipe(minifyCSS())
 		.pipe(rename("styles.min.css"))
 		.pipe(gulp.dest(paths.dist.styles))
-		.on('error', function(err) {
-			gutil.log("went to shit!");
-		});
 });
 
-gulp.task('scripts', function() {
+gulp.task("scripts", function() {
 	gulp.src(paths.app.scripts + "/*.js")
 		.pipe(uglify())
-		.pipe(concat('front-end.min.js'))
+		.pipe(concat("front-end.min.js"))
 		.pipe(gulp.dest(paths.dist.scripts));
 });
 
-gulp.task('images', function () {
+gulp.task("images", function() {
     gulp.src(paths.app.images + "/*")
         .pipe(images({
             progressive: true,
-            svgoPlugins: [{removeViewBox: false}],
-            use: [pngcrush()]
+            svgoPlugins: [ { removeViewBox: false } ],
+            use: [ pngcrush() ]
         }))
         .pipe(gulp.dest(paths.dist.images));
 });
 
-gulp.task('watch', function() {
-	gulp.watch(paths.app.styles + '/*.scss', ['compass']);
-	gulp.watch(paths.app.jade + '/*.jade', ['jade']);
-	gulp.watch(paths.app.fonts + '/*', ['fonts']);
-	gulp.watch(paths.app.scripts + '/*.js', ['scripts']);
-	gulp.watch(paths.app.images + '/*', ['images']);
+gulp.task("watch", function() {
+	gulp.watch(paths.app.styles + "/*.scss", [ "compass" ] );
+	gulp.watch(paths.app.jade + "/*.jade", [ "jade" ] );
+	gulp.watch(paths.app.fonts + "/*", [ "fonts" ] );
+	gulp.watch(paths.app.scripts + "/*.js", [ "scripts" ] );
+	gulp.watch(paths.app.images + "/*", [ "images" ] );
 });
 
-gulp.task('default', [
-	'compass', 
-	'images',
-	'scripts',
-	'fonts',
-	'watch'
+gulp.task("default", [
+	"compass",
+	"images",
+	"scripts",
+	"fonts",
+	"watch"
 ]);
